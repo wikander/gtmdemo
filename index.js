dbef0c102030450a8d0ddcf091fb145d9e32e899
Simon Wikander <simonwikander@gmail.com>
2025-03-10 16:26:11 +0100

    wip

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-document.getElementById("one").addEventListener("click", () => {
-  console.info("btn #one");
-  window.dataLayer.push({
-    event: "btn-#one",
-    myData: {
-      name: "John Doe",
-      age: 30,
-      city: "New York",
-      state: "NY",
-      children: ["Alice", "Bob"],
-    },
+document.querySelectorAll("[data-gtm-id]").forEach((el) => {
+  el.addEventListener("click", () => {
+    const gtmId = `${el.getAttribute("data-gtm-id")}`;
+    console.info(`btn ${gtmId} clicked`);
+    window.dataLayer.push({
+      event: `btn-#${gtmId}`,
+      myData: {
+        name: "John Doe",
+        age: 30,
+        city: "New York",
+        state: "NY",
+        children: ["Alice", "Bob"],
+      },
+    });
   });
 });
