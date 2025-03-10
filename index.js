91c5cc5e23169cfb224c5c5e8a58735d0b7a7804
Simon Wikander <simonwikander@gmail.com>
2025-03-10 16:53:00 +0100

    wip

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const buttonTimesClicked = new Map();
+
 document.querySelectorAll("[data-gtm-id]").forEach((el) => {
+  const gtmId = `${el.getAttribute("data-gtm-id")}`;
+  setButtonName(el, gtmId);
+
   el.addEventListener("click", () => {
-    const gtmId = `${el.getAttribute("data-gtm-id")}`;
     console.info(`btn ${gtmId} clicked`);
+
     window.dataLayer.push({
       event: `btn-#${gtmId}`,
       myData: {
@@ -10,7 +15,21 @@ document.querySelectorAll("[data-gtm-id]").forEach((el) => {
         city: "New York",
         state: "NY",
         children: ["Alice", "Bob"],
+        clickCount: buttonTimesClicked.get(gtmId),
       },
     });
+
+    setButtonName(el, gtmId);
   });
 });
+
+function setButtonName(button, id) {
+  const timesClicked = buttonTimesClicked.get(id);
+
+  let newTimesClicked = timesClicked + 1;
+  if (timesClicked === undefined) {
+    newTimesClicked = 0;
+  }
+  buttonTimesClicked.set(id, newTimesClicked);
+  button.textContent = `Button ${id} | Clicked ${newTimesClicked} times`;
+}
