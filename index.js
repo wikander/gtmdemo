e1bf95c9b79c3e452b74cb87cecdc9a46058f598
Simon Wikander <simonwikander@gmail.com>
2025-03-11 10:37:32 +0100

    wip

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
 const buttonTimesClicked = new Map();
 
+document.getElementById("btn-datalayer-reset").addEventListener("click", () => {
+  console.info("datalayer reset");
+  dataLayer.push(function () {
+    this.reset();
+  });
+});
+
+document
+  .getElementById("btn-datalayer-set-null")
+  .addEventListener("click", () => {
+    console.info("datalayer set myData to null");
+    dataLayer.window.dataLayer.push(function () {
+      this.set("myData", null);
+    });
+  });
+
 document.querySelectorAll("[data-gtm-id]").forEach((el) => {
   const gtmId = `${el.getAttribute("data-gtm-id")}`;
   setButtonName(el, gtmId);
 
   el.addEventListener("click", () => {
     console.info(`btn ${gtmId} clicked`);
-
-    window.dataLayer.push({
-      event: `btn-#${gtmId}`,
-      myData: {
-        name: "John Doe",
-        age: 30,
-        city: "New York",
-        state: "NY",
-        children: ["Alice", "Bob"],
-        clickCount: buttonTimesClicked.get(gtmId),
-      },
-    });
+    const event = `btn-#${gtmId}`;
+    switch (gtmId) {
+      case "one":
+        window.dataLayer.push({
+          event,
+          myData: {
+            name: "John Doe",
+            age: 30,
+            city: "New York",
+            state: "NY",
+            children: ["Anna", "Lena"],
+          },
+        });
+        break;
+      case "two":
+        window.dataLayer.push({
+          event,
+          myData: {
+            name: "Jane Doe",
+            age: 35,
+            gender: "female",
+            city: "Los Angeles",
+            children: ["AnnaLena"],
+          },
+        });
+        break;
+    }
 
     setButtonName(el, gtmId);
   });
